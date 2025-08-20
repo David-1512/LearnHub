b6c757c333c087222a564616ea4fedc08cb65abd
David Serrano <davidjserranom76@gmail.com>
2025-08-20 17:52:39 -0600

    feat(backend): bootstrap Spring Boot app

---
+++ backend/learnHub_backend/src/main/java/com/tutoriasapp/tutorias_api/LearnHubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tutoriasapp.tutorias_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnHubBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnHubBackendApplication.class, args);
+    }
+
+}
---
+++ backend/learnHub_backend/src/test/java/com/tutoriasapp/tutorias_api/LearnHubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutoriasapp.tutorias_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnHubBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
